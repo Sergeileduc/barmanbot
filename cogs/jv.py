220b33c1875a45ffd0c38eaf0aa54fb9dab50e90
Sergeileduc <sergei.leduc@gmail.com>
2023-04-07 15:58:45 +0200

    command with buttons

--- cogs/jv.py
+++ cogs/jv.py
@@ -3,14 +3,15 @@
 """JV cog."""
 import logging
 import re
-from datetime import date
+from datetime import date, timedelta
 from urllib.parse import urljoin
 
 import aiohttp
 from bs4 import BeautifulSoup, Tag
 from dateparser.date import DateDataParser
-from discord import Embed
+from discord import Embed, Interaction
 from discord.ext import commands
+from discord.ui import Button, View
 
 logger = logging.getLogger(__name__)
 
@@ -18,6 +19,11 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
     }
 ddp = DateDataParser(languages=["fr"])
+
+DAY = timedelta(days=1)
+WEEK = timedelta(days=7)
+MONTH = timedelta(days=31)
+QUARTER = timedelta(days=91)
 
 
 class NewGame:
@@ -73,6 +79,10 @@ def generate_url(month: int, year: int):
     return url, french_m, year
 
 
+def next_month(month: int, year: int):
+    return (month + 1, year) if month != 12 else (1, year + 1)
+
+
 async def fetch_page(url: str):
     """Fetch a page on JV, for month releases. If pagination, return the next url.
 
@@ -103,14 +113,83 @@ async def fetch_page(url: str):
     return releases, pages, url
 
 
+async def fetch_month(url):
+    """Fetch all games in a month, even if there are several pages."""
+    pages = True
+    games = []
+    while pages:
+        games_page, pages, url = await fetch_page(url)
+        games += games_page
+    return games
+
+
+async def fetch_time_delta(delta: timedelta):
+    """Fetch games in a time delta relative to today (one week, one month, etc...)"""
+    today = date.today()
+    int_month = today.month
+    int_year = today.year
+
+    url, _, _ = generate_url(today.month, today.year)
+    games = await fetch_month(url)
+    # next month
+    new_month, new_year = next_month(int_month, int_year)
+    url, _, _ = generate_url(new_month, new_year)
+    games += await fetch_month(url)
+    return [game for game in games
+            if (diff := game.date - today) <= delta and diff.days >= 0]
+
+
 class JV(commands.Cog):
     """Fetch Video games release date."""
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @commands.hybrid_command()
-    async def sorties(self, ctx: commands.Context, month: int, year: int):
+    @ commands.hybrid_command()
+    async def sorties(self, ctx: commands.Context):
+        button1 = Button(label="Jour")
+        button2 = Button(label="Semaine")
+        button3 = Button(label="Mois")
+
+        async def day_callback(interraction: Interaction):
+            embed = Embed(title="Sorties du jour")
+            games = await fetch_time_delta(DAY)
+            for game in games:
+                embed.add_field(name=game.name,
+                                value=f"{game.release}\n{game.platforms}\n{game.url}",
+                                inline=False)
+            await interraction.response.send_message(embed=embed)
+
+        async def week_callback(interraction: Interaction):
+            embed = Embed(title="Sorties de la semaine")
+            games = await fetch_time_delta(WEEK)
+            for game in games:
+                embed.add_field(name=game.name,
+                                value=f"{game.release}\n{game.platforms}\n{game.url}",
+                                inline=False)
+            await interraction.response.send_message(embed=embed)
+
+        async def month_callback(interraction: Interaction):
+            embed = Embed(title="Sorties du mois")
+            games = await fetch_time_delta(MONTH)
+            for game in games:
+                embed.add_field(name=game.name,
+                                value=f"{game.release}\n{game.platforms}\n{game.url}",
+                                inline=False)
+            await interraction.response.send_message(embed=embed)
+
+        button1.callback = day_callback
+        button2.callback = week_callback
+        button3.callback = month_callback
+
+        view = View()
+        view.add_item(button1)
+        view.add_item(button2)
+        view.add_item(button3)
+        await ctx.send(view=view)
+
+    @ commands.hybrid_command()
+    async def sorties2(self, ctx: commands.Context, month: int, year: int):
         """Sorties JV
 
         Args:
@@ -119,13 +198,11 @@ class JV(commands.Cog):
         """
         url, month, year = generate_url(month, year)
         embed = Embed(title=f"Sorties du mois {month} {year}")
-        pages = True
-        while pages:
-            games, pages, url = await fetch_page(url)
-            for game in games:
-                embed.add_field(name=game.name,
-                                value=f"{game.release}\n{game.platforms}\n{game.url}",
-                                inline=False)
+        games = await fetch_month(url)
+        for game in games:
+            embed.add_field(name=game.name,
+                            value=f"{game.release}\n{game.platforms}\n{game.url}",
+                            inline=False)
         await ctx.send(embed=embed)
 
 
